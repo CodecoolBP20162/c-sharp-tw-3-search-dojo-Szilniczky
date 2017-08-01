d4a437f046bab0ef78615c97ee6b09b0429838ec
Szilniczky <szilniczky.david@gmail.com>
2017-08-01 10:36:49 +0200

    files added

---
+++ SearchDojo/SearchDojo/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace SearchDojo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPattern = new System.Windows.Forms.TextBox();
+            this.MatchBox = new System.Windows.Forms.ListBox();
+            this.TextBox = new System.Windows.Forms.ListBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(42, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Text";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(374, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Match";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(96, 325);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Pattern:";
+            // 
+            // txtPattern
+            // 
+            this.txtPattern.Location = new System.Drawing.Point(163, 325);
+            this.txtPattern.Name = "txtPattern";
+            this.txtPattern.Size = new System.Drawing.Size(283, 20);
+            this.txtPattern.TabIndex = 5;
+            // 
+            // MatchBox
+            // 
+            this.MatchBox.FormattingEnabled = true;
+            this.MatchBox.Location = new System.Drawing.Point(377, 81);
+            this.MatchBox.Name = "MatchBox";
+            this.MatchBox.Size = new System.Drawing.Size(243, 173);
+            this.MatchBox.TabIndex = 6;
+            // 
+            // TextBox
+            // 
+            this.TextBox.FormattingEnabled = true;
+            this.TextBox.Items.AddRange(new object[] {
+            "Dávid, Mátyás, Ádám, Péter, Mario"});
+            this.TextBox.Location = new System.Drawing.Point(45, 81);
+            this.TextBox.Name = "TextBox";
+            this.TextBox.Size = new System.Drawing.Size(237, 173);
+            this.TextBox.TabIndex = 7;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(480, 323);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(104, 23);
+            this.btnSearch.TabIndex = 8;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 407);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.TextBox);
+            this.Controls.Add(this.MatchBox);
+            this.Controls.Add(this.txtPattern);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Searcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPattern;
+        private System.Windows.Forms.ListBox MatchBox;
+        private System.Windows.Forms.ListBox TextBox;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
